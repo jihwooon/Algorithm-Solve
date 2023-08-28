9fb942c2424c59bd7c64d4f660df0b45822ac3a0
jihwooon <jihwooon@gmail.com>
2023-08-28 15:37:10 +0900

    가운데 문자 출력 문제에 substring을 slice로 변경하라
    slice 메서드는 substring과 동일한 기능을 가지고 있습니다.
    하지만 음수인 인수를 전달 할 수 있는 기능이 있어서
    음수를 전달하면 대상의 문자열 가장 뒤에서부터 시작해 문자열을 잘래내어
    반환합니다.
    
    const str = 'Hello world'
    str.substring(0, 5) // 'Hello'
    str.slice(0, 5) // 'Hello'
    
    str.substring(-5) // 'Hello world'
    str.slice(-5) // 'world'

--- coding-test-javascript/자바스크립트알고리즘문제풀이/src/가운데문자출력.test.js
+++ coding-test-javascript/자바스크립트알고리즘문제풀이/src/가운데문자출력.test.js
@@ -17,9 +17,9 @@ const run = (str) => {
   let answer = '';
   let mid = Math.floor(str.length / 2);
   if (str.length % 2 === 1) {
-    answer = str.substring(mid, mid + 1);
+    answer = str.slice(mid, mid + 1);
   } else {
-    answer = str.substring(mid - 1, mid + 1);
+    answer = str.slice(mid - 1, mid + 1);
   }
   return answer;
 };
